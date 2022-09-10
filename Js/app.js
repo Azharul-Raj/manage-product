f10c8d49a1e34e071bdceb31d1798f5bee088c54
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-09-10 21:20:29 +0600

    work done

--- Js/app.js
+++ Js/app.js
@@ -21,25 +21,39 @@ const setToStorage = (product,quantity) => {
     const stringifiedProduct = JSON.stringify(productObj)
     localStorage.setItem('all_products',stringifiedProduct)
 }
+// display product function
+const display = (product,quantity) => {
+    const productContainer = document.getElementById('all-products')
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <div class="shadow-sm p-3 mb-2 bg-body rounded">
+            <span class="fs-4">${product}</span>
+            Quantity:<small class="fw-bold">
+                ${quantity}
+            </small>
+        </div>
+        `
+        productContainer.appendChild(div)
+}
 // btn event handler function here
 const addProduct = () => {
     const productName = getInputValue('product-name')
     const productQuantity = getInputValue('product-quantity')
     if (isNaN(productName) && !isNaN(productQuantity)) {
-        const productContainer = document.getElementById('all-products')
-        const div = document.createElement('div')
-        div.innerHTML = `
-        <div class="shadow-sm p-3 mb-2 bg-body rounded">
-            <span class="fs-1">${productName}</span>
-            Quantity:<small class="fw-bold">
-                ${productQuantity}
-            </small>
-        </div>
-        `
-        productContainer.appendChild(div)
-
+        display(productName,productQuantity)
+        setToStorage(productName,productQuantity)
     }
     else {
         alert(`PRODUCT NAME CAN'T BE NUMBER AND PRODUCT QUANTITY CAN'T BE STRING`)
     }
 }
+// get product from storage function
+const renderToDisplay = () => {
+    const storedObj = JSON.parse(localStorage.getItem('all_products'))
+    // document.getElementById('all-producs').textContent =` `
+    for (product in storedObj) {
+        const quantity = storedObj[product]
+        display(product,quantity)
+    }
+}
+renderToDisplay()
