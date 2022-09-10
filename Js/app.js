6e4bd214d08c8bed5988ff61ef9464b881d3975e
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-09-10 19:10:19 +0600

    input value taking function done

--- Js/app.js
+++ Js/app.js
@@ -1,1 +1,10 @@
-console.log('app linked')
+// input value taking function
+const getInputValue = (id) => {
+    const inputField = document.getElementById(id)
+    inputField.value = ``
+    return inputField.value
+}
+// btn event handler function here
+const addProduct = () => {
+    const productName = getInputValue('product-name')
+}
